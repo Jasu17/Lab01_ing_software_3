6b50d8698144ecee05d4b06a92ad6268fc86cc83
Javier <jaasmu.9@gmail.com>
2026-08-21 19:16:02 -0500

    update base arch

--- DemoAcademicoApplication/src/main/java/co/edu/demoacademico/controller/EstudianteController.java
+++ DemoAcademicoApplication/src/main/java/co/edu/demoacademico/controller/EstudianteController.java
@@ -1,4 +1,28 @@
 package co.edu.demoacademico.controller;
+import co.edu.demoacademico.model.Estudiante;
+import co.edu.demoacademico.service.EstudianteService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/estudiantes")
 public class EstudianteController {
+
+    private final EstudianteService service;
+
+    public EstudianteController(EstudianteService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Estudiante crear(@Valid @RequestBody Estudiante estudiante) {
+        return service.crear(estudiante);
+    }
+
+    @GetMapping
+    public List<Estudiante> listar() {
+        return service.listar();
+    }
 }
